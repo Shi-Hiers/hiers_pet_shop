579588c8864af1765f642c08c40dc44812b35e6e
Shi-Hiers <shidericahiers03@gmail.com>
2024-09-19 15:36:12 -0400

    pet

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,26 @@
-# Import the render_template function from the flask module
 from flask import render_template
-
-# Import the app instance from the current package
 from . import app
 
-# Define the route for the home page
 @app.route('/')
-def index():
-    # Render the index.html template
+def home():
     return render_template('index.html')
 
-# Define the route for the about page
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
 @app.route('/about')
 def about():
-    # Render the about.html template
     return render_template('about.html')
 
-# Define the route for the exam page
 @app.route('/exam')
 def exam():
-    # Render the exam.html template
     return render_template('exam.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/informative')
+def informative():
+    return render_template('informative.html')
